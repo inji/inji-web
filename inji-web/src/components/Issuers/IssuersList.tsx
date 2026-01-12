c68224bbbcc1767c379ff6dc56815bc3e47b9813
sanehema9 <sanehema9@gmail.com>
2026-01-12 10:06:20 +0530

    fix/INJIWEB-1789-isserspageupdate
    Signed-off-by: sanehema9 <sanehema9@gmail.com>

--- inji-web/src/components/Issuers/IssuersList.tsx
+++ inji-web/src/components/Issuers/IssuersList.tsx
@@ -29,8 +29,8 @@ export const IssuersList: React.FC<IssuersListProps> = ({state}) => {
     <div data-testid="Issuers-List-Container" className="flex flex-col items-center justify-center">
             <HeaderTile content={t("containerHeading")} subContent={t("containerSubHeading")}/>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4 sm:p-6 md:p-8">
-        {issuers.filtered_issuers.map((issuer: IssuerObject) => (
-          <Issuer key={issuer.issuer_id} issuer={issuer} />
+        {issuers.filtered_issuers.map((issuer: IssuerObject, index: number) => (
+         <Issuer key={issuer.issuer_id} issuer={issuer} index={index} />
         ))}
             </div>
         </div>
